41d1a340c4b96426fa1bb98bd433828bdd60c748
Arden Johannes Sasan <ajsasan@dnamicro.com>
2021-11-02 07:29:06 +0800

    fixed bug in path

--- src/common/enums/path.ts
+++ src/common/enums/path.ts
@@ -1,10 +1,10 @@
 export enum Path{
-    Home = "movie-catalogue/home",
-    HomePage = "movie-catalogue/",
-    Root ="movie-catalogue/",
-    Movies ="movie-catalogue/movies/",
-    Movie = "movie-catalogue/movie/",
-    SearchByGenre = "movie-catalogue/search/genre/",
-    SearchByLanguage = "/movie-cataloguesearch/language/",
-    SearchByQuery = "movie-catalogue/search/query/"
+    Home = "/movie-catalogue/home",
+    HomePage = "/movie-catalogue/",
+    Root ="/",
+    Movies ="/movie-catalogue/movies/",
+    Movie = "/movie-catalogue/movie/",
+    SearchByGenre = "/movie-catalogue/search/genre/",
+    SearchByLanguage = "/movie-catalogue/search/language/",
+    SearchByQuery = "/movie-catalogue/search/query/"
 }
