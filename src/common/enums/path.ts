f231132e01439569e50dc40e2b176ac3916f9b67
Arden Johannes Sasan <ajsasan@dnamicro.com>
2021-11-02 08:07:34 +0800

    removed gh-pages

--- src/common/enums/path.ts
+++ src/common/enums/path.ts
@@ -1,10 +1,10 @@
 export enum Path{
-    Home = "/movie-catalogue/home",
+    Home = "/home",
     PageRoot ="/movie-catalogue/",
     Root ="/",
-    Movies ="/movie-catalogue/movies/",
-    Movie = "/movie-catalogue/movie/",
-    SearchByGenre = "/movie-catalogue/search/genre/",
-    SearchByLanguage = "/movie-catalogue/search/language/",
-    SearchByQuery = "/movie-catalogue/search/query/"
+    Movies ="/movies/",
+    Movie = "/movie/",
+    SearchByGenre = "search/genre/",
+    SearchByLanguage = "/search/language/",
+    SearchByQuery = "/search/query/"
 }
